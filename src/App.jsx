84c3ba5859f4570b06a9ce37e80dea66a63ca7da
0Suryansh <suryanshsrv@gmail.com>
2023-11-15 18:09:25 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Main from './pages/Main'
 import { Provider } from 'react-redux'
-import store from './store'
+import {store, persistor} from './store'
+import { PersistGate } from 'redux-persist/integration/react';
+
 function App() {
+  useEffect(() => {
+    console.clear();
+  });
 
   return (
     <>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <Main/>
+        </PersistGate>
       </Provider>
     </>
   )
